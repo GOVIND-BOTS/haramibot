3131d43d99a740774eb07a934b20ab174fab958f
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-10-29 20:44:23 +0530

    Create welcome.py

---
+++ AnonX/plugins/welcome.py
@@ -0,0 +1,46 @@
+import random
+from pyrogram import Client
+from pyrogram.types import Message
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from Anonx import app  
+
+photo = [
+    "https://telegra.ph/file/1b819cfbcb2a2d3c738f6.jpg",
+    "https://telegra.ph/file/3021c823c7f006658682f.jpg",
+    "https://telegra.ph/file/05561f0fbf323e057ab87.jpg",
+    "https://telegra.ph/file/7a6b51ee0077724254ca7.jpg",
+    "https://telegra.ph/file/b3de9e03e5c8737ca897f.jpg",
+    "https://telegra.ph/file/0b6bb91986ef3a143033b.jpg",
+    "https://telegra.ph/file/2b5b66c9a0989afa0779a.jpg",
+    "https://telegra.ph/file/471339bb1901a007c0c2f.jpg",
+    "https://telegra.ph/file/ab7d958d707ef649bc3c3.jpg",
+    "https://telegra.ph/file/4f877f2843f31fcc32242.jpg",
+    "https://telegra.ph/file/ecefaa3e00fb911826673.jpg",
+    "https://telegra.ph/file/2a10683a2166f7cf4940b.jpg",
+    "https://telegra.ph/file/cda35ca740e1229626e48.jpg",
+    "https://telegra.ph/file/9f7186cd3d87199426e03.jpg",
+    "https://telegra.ph/file/7fe39f1baa1a93b9a3f0e.jpg",
+    
+]
+
+
+@app.on_message(filters.new_chat_members, group=3)
+async def join_watcher(_, message):    
+    chat = message.chat
+    
+    for members in message.new_chat_members:
+        
+            count = await app.get_chat_members_count(chat.id)
+
+            msg = (
+                f"**😇𝐡𝐞𝐲 {message.from_user.mention} 𝚠𝚎𝚕𝚌𝚘𝚖𝚎 𝚒𝚗 𝚊 𝚗𝚎𝚠 𝚐𝚛𝚘𝚞𝚙🥳**\n\n"
+                f"**📝𝗀𝗋𝗈𝗎𝗈 𝗇𝖺𝗆𝖾:** {message.chat.title}\n➖➖➖➖➖➖➖➖➖➖➖\n"
+                f"**🔐𝗰𝗵𝗮𝘁 𝘂𝘀𝗲𝗿𝗻𝗮𝗺𝗲:** @{message.chat.username}\n➖➖➖➖➖➖➖➖➖➖➖\n"
+                f"**💖𝗨𝗥 𝗜𝗗:** {message.from_user.id}\n➖➖➖➖➖➖➖➖➖➖➖\n"
+                f"**✍️𝗨𝗥 𝘂𝘀𝗲𝗿𝗻𝗮𝗺𝗲:** @{message.from_user.username}\n➖➖➖➖➖➖➖➖➖➖➖\n"
+                f"**👥𝘁𝗼𝘁𝗲𝗹 {count} 𝗺𝗲𝗿𝗯𝗲𝗿𝘀🎉**"
+            )
+            await app.send_photo(message.chat.id, photo=random.choice(photo), caption=msg, reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(f"🥳𝐤𝐢𝐝𝐧𝐚𝐩 𝐦𝐞🥳", url=f"https://t.me/{app.username}?startgroup=true")]
+         ]))
