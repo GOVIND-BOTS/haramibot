cd64c1754080c9589adc48b4bdb9f53775b6fe00
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-11-02 07:40:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ SONG_DOWNLOAD_DURATION = int(
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001919162625"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001966336198"))
 
 # A name for your Music bot.
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "GOVIND MUSIC")
