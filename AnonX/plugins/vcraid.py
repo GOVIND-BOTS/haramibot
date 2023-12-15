018f16021eb56660c02c53eceb70a07ff41bc8fd
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-12-15 19:21:01 +0530

    Update vcraid.py

--- AnonX/plugins/vcraid.py
+++ AnonX/plugins/vcraid.py
@@ -34,7 +34,7 @@ aud_list = [
 
 
 
-@AnonX.on_message(filters.user(SUDO_USERS) & filters.command(["vcraid"], prefixes=HNDLR))
+@Flame.on_message(filters.user(SUDO_USERS) & filters.command(["vcraid"], prefixes=HNDLR))
 async def vcraid(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
@@ -60,7 +60,7 @@ async def vcraid(_, e: Message):
             await e.reply_text(f"**> Raiding in:** {chat.title} \n\n**> Audio:** {songname} \n**> Position:** Ongoing Raid")
 
 
-@AnonX.on_message(filters.user(SUDO_USERS) & filters.command(["vraid"], prefixes=HNDLR))
+@Flame.on_message(filters.user(SUDO_USERS) & filters.command(["vraid"], prefixes=HNDLR))
 async def vraid(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
@@ -88,7 +88,7 @@ async def vraid(_, e: Message):
             await e.reply_text(f"**> Raiding in:** {chat.title} \n\n**> Video:** {songname} \n**> Position:** Ongoing Raid")
 
 
-@AnonX.on_message(filters.user(SUDO_USERS) & filters.command(["raidend"], prefixes=HNDLR))
+@Flame.on_message(filters.user(SUDO_USERS) & filters.command(["raidend"], prefixes=HNDLR))
 async def ping(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
@@ -109,7 +109,7 @@ async def ping(_, e: Message):
         await e.reply_text("**No ongoing raid!**")
 
 
-@AnonX.on_message(filters.user(SUDO_USERS) & filters.command(["raidpause"], prefixes=HNDLR))
+@Flame.on_message(filters.user(SUDO_USERS) & filters.command(["raidpause"], prefixes=HNDLR))
 async def ping(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
@@ -130,7 +130,7 @@ async def ping(_, e: Message):
         await e.reply_text("**No ongoing raid!**")
 
 
-@AnonX.on_message(filters.user(SUDO_USERS) & filters.command(["raidresume"], prefixes=HNDLR))
+@Flame.on_message(filters.user(SUDO_USERS) & filters.command(["raidresume"], prefixes=HNDLR))
 async def ping(_, e: Message):
     gid = e.chat.id
     uid = e.from_user.id
