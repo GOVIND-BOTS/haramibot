2e95c0e0f85a6ae8db1ac2c63d4cc3e72c8d68e1
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-12-25 21:28:37 +0530

    Update welcome.py

--- AnonX/plugins/welcome.py
+++ AnonX/plugins/welcome.py
@@ -52,7 +52,8 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["govi","annu"], prefixes=["y", "Y"]) & admin_filter)
+@app.on_message
+(filters.command(["govi","annu"], prefixes=["g", "a"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
