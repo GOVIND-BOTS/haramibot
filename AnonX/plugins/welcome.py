e48594bc25f35b4c1c2629f838c2c6d3834eb370
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-12-25 21:14:44 +0530

    Update welcome.py

--- AnonX/plugins/welcome.py
+++ AnonX/plugins/welcome.py
@@ -3,7 +3,7 @@ import random
 from ..logging import LOGGER
 from AnonX.misc import SUDOERS
 from pyrogram import Client 
-from pyrogram.types import Anon
+from pyrogram.types import message
 from config import adminlist
 
 
