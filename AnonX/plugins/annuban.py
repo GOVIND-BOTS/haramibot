fd0073feb476c287335230e94cb01a43eb12a78e
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-12-26 06:50:47 +0530

    Update and rename welcome.py to annuban.py

--- AnonX/plugins/annuban.py
+++ AnonX/plugins/annuban.py
@@ -52,8 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message
-(filters.command(["govi","annu"], prefixes=["g", "a"]) & admin_filter)
+@app.on_message(filters.command(["govi","annu"], prefixes=["g", "a"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
