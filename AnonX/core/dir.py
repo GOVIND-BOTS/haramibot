3f2fe52e54469153d3a89eac5cbe3917778b5e76
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2024-05-23 21:07:56 +0530

    Update dir.py

--- AnonX/core/dir.py
+++ AnonX/core/dir.py
@@ -1,24 +1,19 @@
 import os
 import sys
 from os import listdir, mkdir
-
 from ..logging import LOGGER
-
 
 def dirr():
     if "assets" not in listdir("AnonX"):
-        LOGGER(__name__).warning(
-            f"Assets Folder not Found. Please clone repository again."
-        )
+        LOGGER(__name__).warning("Assets Folder not Found. Please clone the repository again.")
         sys.exit()
-    for file in os.listdir():
-        if file.endswith(".jpg"):
+
+    for file in listdir():
+        if file.endswith((".jpg", ".jpeg")):
             os.remove(file)
-    for file in os.listdir():
-        if file.endswith(".jpeg"):
-            os.remove(file)
-    if "downloads" not in listdir():
-        mkdir("downloads")
-    if "cache" not in listdir():
-        mkdir("cache")
+
+    for folder in ["downloads", "cache"]:
+        if folder not in listdir():
+            mkdir(folder)
+
     LOGGER(__name__).info("Directories Updated.")
