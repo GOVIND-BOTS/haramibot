8650c5fd9b410c71cf6d53ccdebd9ca25f4efb47
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2024-05-23 07:50:24 +0530

    Update logging.py

--- AnonX/logging.py
+++ AnonX/logging.py
@@ -1,24 +1,24 @@
 import logging
 from logging.handlers import RotatingFileHandler
-
 from config import LOG_FILE_NAME
 
+# Define the logging configuration
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
         RotatingFileHandler(
-            LOG_FILE_NAME, maxBytes=5000000, backupCount=10
+            LOG_FILE_NAME, maxBytes=5000000, backupCount=10, encoding='utf-8'
         ),
         logging.StreamHandler(),
     ],
 )
 
+# Set log level to ERROR for noisy loggers
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
-
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
