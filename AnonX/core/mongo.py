f7c49d0fa655fe56cc355139dd821b4f2205356a
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2024-05-23 21:19:54 +0530

    Update mongo.py

--- AnonX/core/mongo.py
+++ AnonX/core/mongo.py
@@ -1,12 +1,13 @@
-from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
-from pymongo import MongoClient
+from aiosqlite import connect as _mongo_client_
+from aiosqlite import Database
 
 import config
 
 from ..logging import LOGGER
 
-
-_mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-_mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-mongodb = _mongo_async_.Vip
-pymongodb = _mongo_sync_.Vip
+async def connect_to_mongodb():
+    _mongo_async_ = await _mongo_client_(config.MONGO_DB_URI)
+    _mongo_sync_ = await _mongo_client_(config.MONGO_DB_URI)
+    mongodb = Database(_mongo_async_, 'Vip')
+    pymongodb = Database(_mongo_sync_, 'Vip')
+    return mongodb, pymongodb
